770ed185c5d0acb967d40710ce13512de7e87236
JuneWool <junewoole@gmail.com>
2019-05-19 02:02:50 +0800

    banner

--- emall/pages/index/index.js
+++ emall/pages/index/index.js
@@ -9,7 +9,17 @@ Page({
     autoplay: true,
     interval: 5000,
     duration: 1000,
-    circular: true
+    circular: true,
+    nav: [],
+    scrollX: true,
+    currentNav: "",
+    toNav: ""
+  }, 
+  tapNav: function(e) {
+    console.log(e)
+    this.setData({
+      currentNav: e.currentTarget.id
+    })
   },
   onLoad: function () {
     this.setData({
@@ -17,7 +27,18 @@ Page({
         'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
         'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
         'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
-        ]
+      ],
+      nav: [
+        {id: "all", name: "全部"},
+        {id: "food", name: "食品"},
+        {id: "clothes", name: "服装"},
+        {id: "goods", name: "日用品"},
+        {id: "souvenir", name: "纪念品"},
+        {id: "makeup", name: "化妆品"},
+        {id: "others", name: "其他"}
+      ],
+      currentNav: "all",
+      toNav: "all"
     })
   }
 })
